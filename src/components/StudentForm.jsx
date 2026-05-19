bab67b8b24775643a21436c1ca7c11152eb14574
Jed Aeron Torres <jedaeront24@gmail.com>
2026-05-19 17:46:53 +0800

    Updated portal design

--- src/components/StudentForm.jsx
+++ src/components/StudentForm.jsx
@@ -1,16 +1,45 @@
-// StudentForm.jsx
+import { useState } from "react";
 
 function StudentForm() {
+
+  const [name, setName] = useState("");
+  const [course, setCourse] = useState("");
+
+  const handleSave = (e) => {
+    e.preventDefault();
+
+    if (name === "" || course === "") {
+      alert("Please fill in all fields!");
+      return;
+    }
+
+    alert("Successfully Added Student!");
+
+    // clear inputs after adding
+    setName("");
+    setCourse("");
+  };
+
   return (
     <section className="form-section">
 
       <h2>Add Student</h2>
 
-      <form>
+      <form onSubmit={handleSave}>
 
-        <input type="text" placeholder="Full Name" />
+        <input
+          type="text"
+          placeholder="Full Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
 
-        <input type="text" placeholder="Course" />
+        <input
+          type="text"
+          placeholder="Course"
+          value={course}
+          onChange={(e) => setCourse(e.target.value)}
+        />
 
         <button type="submit">
           Add Student
@@ -19,7 +48,7 @@ function StudentForm() {
       </form>
 
     </section>
-  )
+  );
 }
 
-export default StudentForm
+export default StudentForm; 
